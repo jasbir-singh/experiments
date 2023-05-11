3d8c2770f9ce78e377318056bb1c59f7c7d10e45
Jasbir Singh <jasbir_singh@live.co.uk>
2023-05-11 15:43:25 +0100

    Generate a new IOS app

---
+++ Swara/Swara/SwaraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwaraApp.swift
+//  Swara
+//
+//  Created by Jasbir Singh on 11/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwaraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
